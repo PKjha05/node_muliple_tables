de7ade0badcc9c5968d7724acf7ddde18c35870c
PKjha05 <pkjha05122001@gmail.com>
2023-04-04 11:11:46 +0530

    first Commit

--- controllers/customerControllers.js
+++ controllers/customerControllers.js
@@ -1,16 +1,13 @@
 import sequelize from "../config/dbconfig.js";
 import customer from "../models/customer.js";
 import customer_address from "../models/customerAddress.js";
-export const getData = async (req, res)=>
-{
-    try{
-    const users = await customer_address.findAll();
-    return res.status(200).json({message: "success", data:users});
-    }
-    catch(error)
-    {
-        console.log(error);
-    }
-      
-
+export const getData = async (req, res) => {
+  try {
+    const users = await sequelize.query(
+      "SELECT * FROM customer JOIN customer_address ON customer.customerId = customer_address.customerId"
+    );
+    return res.status(200).json({ message: "success", data: users });
+  } catch (error) {
+    console.log(error);
+  }
 };
